9e72786d1f276ad73d548006b2a93e1ffe5f2519
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-04 13:18:15 -0800

    added two pop tests

--- test_linked_list.py
+++ test_linked_list.py
@@ -10,8 +10,22 @@ def test_value_inserts_at_the_head():
     assert test_list.head.val == "test"
 
 
-def test_pop():
-    pass
+def test_pop_returns_correct_head():
+    test_list = LinkedList()
+    test_list.insert('test')
+
+    assert test_list.pop().val == "test"
+
+def test_pop_updates_head():
+    test_list = LinkedList()
+    test_list.insert('test')
+    test_list.insert('test2')
+
+    test_list.pop()
+
+    assert test_list.head.val == "test"
+
+
 
 def test_size():
     pass
