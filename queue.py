a7cb0fb3acdee0256cd1fc950adf8b00d02a57bb
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-09 14:02:57 -0800

    linter whitespace.

--- queue.py
+++ queue.py
@@ -2,7 +2,7 @@
 
 
 class Queue(object):
-    
+
     def __init__(self):
         self.front = None
         self.back = None
@@ -15,17 +15,14 @@ class Queue(object):
         except AttributeError:
             self.front = new_item
         self.back = new_item
-        
-        
-        
-        
+
     def dequeue(self):
         """Remove the front item from the queue and return its value"""
         pass
 
 
 class Item(object):
-    
+
     def __init__(self, val, next_item=None):
         self.val = val
         self.next_item = next_item
