4bbeeb89489387cd80642e0b1f683877c0585033
henrykh <henryhowes@gmail.com>
2015-02-11 19:20:36 -0800

    adds docstrings, updates README

--- binary_heap.py
+++ binary_heap.py
@@ -2,9 +2,13 @@
 
 
 class BinaryHeap(object):
-    """Implement a binary heap data structure"""
+    """Implements a binary max-heap data structure"""
 
     def __init__(self, an_iter=None):
+        '''Initializes the heap,
+        optionally populates it with the given iterable
+        '''
+
         self._list = []
 
         if an_iter:
@@ -12,6 +16,10 @@ class BinaryHeap(object):
                 self.push(val)
 
     def push(self, val):
+        '''Pushes the value into the heap,
+         adjusting the ordering to maintain the heap
+         '''
+
         self._list.append(val)
         child_pos = len(self._list) - 1
 
@@ -23,6 +31,10 @@ class BinaryHeap(object):
             child_pos = (child_pos - 1) // 2
 
     def pop(self):
+        '''Pops the top value off the heap and returns it,
+         adjusting the ordering accordingly
+         '''
+
         try:
             self._list[0], self._list[-1] = self._list[-1], self._list[0]
             top = self._list.pop()
@@ -33,7 +45,8 @@ class BinaryHeap(object):
                 larger_child = None
                 larger_child_index = None
                 if len(children_pos) > 1:
-                    if self._list[children_pos[0]] >= self._list[children_pos[1]]:
+                    if self._list[children_pos[0]] >= self._list[
+                            children_pos[1]]:
                         larger_child = self._list[children_pos[0]]
                         larger_child_index = children_pos[0]
                     else:
@@ -55,7 +68,7 @@ class BinaryHeap(object):
             raise IndexError("The heap is empty")
 
     def _switch(self, child_index):
-        """swap the parent and child"""
+        """Helper, swaps the child at the given index with its parent"""
         parent_index = (child_index - 1) // 2
 
         self._list[parent_index], self._list[child_index] = (
@@ -63,6 +76,10 @@ class BinaryHeap(object):
         )
 
     def _find_children(self, parent):
+        '''Helper, returns a list of children for the given parent,
+         an empty list if none
+         '''
+
         children_indices = []
 
         if len(self._list) > 2*parent + 1:
