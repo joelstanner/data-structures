013c76b25e7ff49e3f42bd1a7e2da27f72ae7462
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-17 14:07:18 -0800

    added docstrings, updated readme

--- graph/simple_graph.py
+++ graph/simple_graph.py
@@ -5,9 +5,11 @@ class Graph(object):
         self.graph_dict = {}
 
     def nodes(self):
+        """return a list of all nodes in the graph"""
         return self.graph_dict.keys()
 
     def edges(self):
+        """return a list of all edges in the graph"""
         edge_list = []
         for key, value in self.graph_dict.items():
             for item in value:
@@ -17,9 +19,13 @@ class Graph(object):
         # return [(key, value) for key, value in self.graph_dict.items()]
 
     def add_node(self, node):
+        """add a new node 'n' to the graph"""
         self.graph_dict[node] = []
 
     def add_edge(self, node_1, node_2):
+        """add a new edge to the graph connecting 'n1' and 'n2', if either n1
+        or n2 are not already present in the graph, they are added.
+        """
         try:
             self.graph_dict[node_1].append(node_2)
         except KeyError:
@@ -29,6 +35,7 @@ class Graph(object):
             self.add_node(node_2)
 
     def del_node(self, node):
+        """delete the node 'n' from the graph"""
         try:
             del self.graph_dict[node]
             for val_list in self.graph_dict.values():
@@ -38,6 +45,7 @@ class Graph(object):
             raise KeyError("Node not found")
 
     def del_edge(self, node_1, node_2):
+        """delete the edge connecting 'n1' and 'n2' from the graph"""
         try:
             self.graph_dict[node_1].remove(node_2)
         except KeyError:
@@ -46,15 +54,19 @@ class Graph(object):
             raise ValueError("Edge not found")
 
     def has_node(self, node):
+        """True if node 'n' is contained in the graph, False if not"""
         return node in self.graph_dict
 
     def neighbors(self, node):
+        """return the list of all nodes connected to 'n' by edges"""
         try:
             return self.graph_dict[node]
         except KeyError:
             raise KeyError("Node not found")
 
     def adjacent(self, node_1, node_2):
+        """return True if there is an edge connecting n1 and n2, False if not
+        """
         if not self.has_node(node_2):
             raise KeyError("Second node not found")
         try:
