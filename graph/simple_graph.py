fa2602a5e082a57351509b7cacdaacff8f443496
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-19 16:00:11 -0800

    importing Queue

--- graph/simple_graph.py
+++ graph/simple_graph.py
@@ -1,6 +1,5 @@
 from collections import OrderedDict
-# import Queue
-
+from queue import Queue
 
 class Graph(object):
     """Implements a graph data structure"""
@@ -20,7 +19,7 @@ class Graph(object):
 
     def add_node(self, node):
         """add a new node 'n' to the graph"""
-        self.graph_dict.setdefault(node,[])
+        self.graph_dict.setdefault(node, [])
 
     def add_edge(self, node_1, node_2):
         """add a new edge to the graph connecting 'n1' and 'n2', if either n1
@@ -103,69 +102,3 @@ class Graph(object):
                     queue.enqueue(child)
 
         return explored.keys()
-
-   
-
-
-
-
-
-class Queue(object):
-    """Implement a queue data structure"""
-    def __init__(self):
-        self.front = None
-        self.back = None
-
-    def __iter__(self):
-        current = self.front
-        while current:
-            yield current
-            current = current.next_item
-
-    def enqueue(self, val):
-        """Take item value, add to the back of the queue"""
-        new_item = Item(val)
-        try:
-            self.back.next_item = new_item
-        except AttributeError:
-            self.front = new_item
-        self.back = new_item
-
-    def dequeue(self):
-        """Remove the front item from the queue and return its value"""
-
-        prevFront = self.front
-        try:
-            self.front = self.front.next_item
-            if self.front is None:
-                self.back = self.front
-        except AttributeError:
-            raise AttributeError("The queue is empty")
-        return prevFront.val
-
-    def size(self):
-        count = 0
-        for _ in self:
-            count += 1
-
-        return count
-
-
-class Item(object):
-
-    def __init__(self, val, next_item=None):
-        self.val = val
-        self.next_item = next_item
-
-     # def _breadth_first_traversal(self, start, explored):
-     #    explored.setdefault(start, 1)
-
-     #    for child in self.graph_dict[start]:
-     #        if child not in explored:
-     #            self._depth_first_traversal(child, explored)
-
-     #    return explored.keys()
-
-
-
-
